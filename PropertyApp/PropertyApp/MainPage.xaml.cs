527a474b7b4198a282a5b5781a976af72ac5fbe6
ronald <rmontero@gmail.com>
2021-10-24 12:14:46 -0400

    ...

--- PropertyApp/PropertyApp/MainPage.xaml.cs
+++ PropertyApp/PropertyApp/MainPage.xaml.cs
@@ -4,9 +4,6 @@ using PropertyApp.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace PropertyApp
